5c13d920cae258252e07a2d7232d7a94d5c4fedf
dnnam2610 <nngit2610@gmail.com>
2025-05-23 18:52:28 +0700

    create-def-combine

--- combine_data.py
+++ combine_data.py
@@ -1,22 +1,25 @@
 import pandas as pd
 
-if __name__ == "__main__":
-    true_data = pd.read_csv("DataSet_Misinfo_True.csv", usecols=["text"])
-    fake_data = pd.read_csv("DataSet_Misinfo_Fake.csv", usecols=["text"])
-
-
-    # print(true_data.shape)
-    # print(fake_data.shape)
-
+def combine_data(true_data, fake_data, export=True):
     # Combine data
     true_data["label"] = True
     fake_data["label"] = False
 
+    true_data = true_data.dropna(axis=0)
+    fake_data = fake_data.dropna(axis=0)
+
     combined_data = pd.concat([true_data, fake_data], ignore_index=True)
     combined_data["lenght_text"] = combined_data["text"].str.len()
-    print(combined_data.head(5))
+    if export:
+        combined_data.to_csv("Combined_data.csv", encoding="utf-8")
+    return combined_data
+
+
+if __name__ == "__main__":
     
-    # Export data
-    combined_data.to_csv("Combined_data.csv", encoding="utf-8")
+    true_data = pd.read_csv("DataSet_Misinfo_True.csv", usecols=["text"])
+    fake_data = pd.read_csv("DataSet_Misinfo_Fake.csv", usecols=["text"])
+
+    combined_data = combine_data(true_data=true_data, fake_data=fake_data, export=True)
 
 
